163b914729baa9c65bcef224e26ea9b0dd5b45e8
Alexandre P <alexandreponcet.mail@gmail.com>
2020-04-23 13:45:27 +0200

    first commit with cpu struct

---
+++ gb/cpu.go
@@ -0,0 +1,14 @@
+package gb
+
+type Status uint8
+
+type Registers struct {
+	A uint8
+	B uint8
+	C uint8
+	D uint8
+	E uint8
+	F uint8
+	H uint8
+	L uint8
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/lauzel/gboyemulator/gb"
+)
+
+func main() {
+	
+}
